183926fbcf9d3cac1810d26c9765c52c548e3adc
MARTIN PEREZ Miguel <miguel.martinperez@axa.be>
2020-06-10 12:02:34 +0200

    Add project files.

---
+++ MyHello/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyHello
+{
+
+    class Program
+    {
+    
+        public static void Afficher()
+        {
+            Console.WriteLine("Youhouuu");
+            int a = 5, b = 32;
+            int c = a + b;
+            Console.WriteLine(a + "+" + b + "=" + c);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("How are you today ?");
+            Afficher();
+        }
+    }
+}
