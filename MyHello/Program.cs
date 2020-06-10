f70f1dfef7528cbc188fc72fa495301dffea55c9
MARTIN PEREZ Miguel <miguel.martinperez@axa.be>
2020-06-10 13:14:10 +0200

    This is my new changes

--- MyHello/Program.cs
+++ MyHello/Program.cs
@@ -14,11 +14,27 @@ namespace MyHello
             Console.WriteLine(a + "+" + b + "=" + c);
 
         }
+
+        public static int Addition()
+        {
+            int d = 10;
+            int e = 32;
+            return d + e;
+
+        }
         static void Main(string[] args)
         {
+            
+
             Console.WriteLine("Hello World!");
             Console.WriteLine("How are you today ?");
             Afficher();
+             Afficher();
+            Console.WriteLine("That's all folks!!");
+            // Test
+            int res = Addition();
+            Console.WriteLine("resultat = " + res);
+
         }
     }
 }
